514aa0bbf6aba3794a5356911a15be375f705aa6
Bestony <xiqingongzi@gmail.com>
2016-09-07 00:26:40 +0800

    Revert "Fix \NOS\NosException"
    This reverts commit 727c00aafb77ba9a878234482a701e0caac31104.

--- Plugin.php
+++ Plugin.php
@@ -90,7 +90,7 @@ class Nos_Plugin implements Typecho_Plugin_Interface{
         try{
            $ins->uploadFile($option->bucket,$savename, $filename);
             
-        } catch(\NOS\NosExceptionNosException $e) {
+        } catch(NosException $e) {
             return false;
         }
         
